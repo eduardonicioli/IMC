08a7d6afa20a8afd87f0fe4245ec1866b561e2ef
Carlos Eduardo Nicioli de Toledo <111098632+eduardonicioli@users.noreply.github.com>
2023-10-05 22:05:03 -0300

    Update Final
    Projeto encerrado, porem Git pages não tem suporte a PHP

--- imc.php
+++ imc.php
@@ -6,23 +6,35 @@
     <title>Resultado do IMC</title>
 </head>
 <body>
+    
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtém os valores do peso e altura do formulário
-    $peso = floatval($_POST["peso"]);
-    $altura = floatval($_POST["altura"]);
 
-    // Verifica se os valores são válidos
-    if ($peso <= 0 || $altura <= 0) {
-        echo "<p>Por favor, insira valores válidos para peso e altura.</p>";
-    } else {
-        // Calcula o IMC
-        $imc = $peso / ($altura * $altura);
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
+$imc = $peso / ($altura * $altura);
 
-        // Exibe o resultado do IMC
-        echo "<h3>Seu IMC é: " . number_format($imc, 2) . "</h3>";
-    }
+
+echo "<h2>Seu IMC é: $imc</h2>";
+
+if($imc < 18.5){
+    echo"<h3>Você está abaixo do peso</h3>";
 }
+elseif($imc >= 18.6 && $imc <= 24.99){
+    echo"<h3>Você está no peso ideal!</h3>";
+}
+elseif($imc >= 25.0 && $imc <= 29.99){
+    echo"<h3>Você está levemente sobrepeso</h3>";
+}
+elseif($imc >= 30.0 && $imc <=39.99){
+    echo"<h3>Você está com obesidade I</h3>";
+}
+elseif($imc >=  35.0 && $imc <= 39.0){
+    echo"<h3>Você está com obesidade II (severa)</h3>";
+}
+else{
+    echo"<h3>Você está com obesiadade III (mormida)</h3>";
+}
+
 ?>
 <br>
 <a href="index.html">Voltar</a> <!-- Adicione um link para voltar à página anterior -->
